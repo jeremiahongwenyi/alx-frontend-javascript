921ba09e3e9229b61563b7e9af6e45d817732228
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-11-10 00:31:35 +0300

    correction

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,11 +35,8 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-interface printTeacherFunction {
-  (teacher: { firstName: string; lastName: string }): string;
-}
 
-const printTeacher: printTeacherFunction = ({ firstName, lastName }:{ firstName:string, lastName:string } ):string => {
+function printTeacher ({ firstName, lastName }:{ firstName:string, lastName:string } ):string {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
