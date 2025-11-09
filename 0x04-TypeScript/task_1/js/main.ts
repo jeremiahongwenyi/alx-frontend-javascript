a6bd1dc5c170953b153c2a1c23b667bf0d7c076c
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-11-10 00:19:24 +0300

    correction git push

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,9 +35,14 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+function printTeacher(firstName:string, lastName:string): string{
+return `${firstName.charAt(0)}. ${lastName}`;
+}
+  
+
 
 console.log(printTeacher("John", "Doe"));
+
+
+
 
