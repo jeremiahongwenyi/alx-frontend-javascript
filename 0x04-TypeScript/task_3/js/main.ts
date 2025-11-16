b93c1e0d9760ff5fd12f854b31febe1598d90d0e
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-11-16 16:42:08 +0300

    question 8 correction

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -11,9 +11,8 @@ lastName: "Salva"
 
 const newRowID: RowID = CRUD.insertRow(row)
 
-const updatedRow: RowElement = {
-  ...row, age:23
-}
+const updatedRow: RowElement = { ...row, age: 23 }
+
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
